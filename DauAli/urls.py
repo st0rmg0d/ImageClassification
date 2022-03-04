568329b789b837ee5fe515132b4a87eb0ea47029
st0rmg0d <55540414+st0rmg0d@users.noreply.github.com>
2022-03-04 08:13:08 +0600

    Update urls.py

--- DauAli/urls.py
+++ DauAli/urls.py
@@ -26,9 +26,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='homepage'),
     path('predictImage', views.predictImage, name='predictImage'),
-    
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
